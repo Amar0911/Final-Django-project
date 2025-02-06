f1de1b69cc20b580e1efe0da292859661308f53d
Amar0911 <guptaamar0911@gmail.com>
2025-02-06 15:10:44 +0530

    updates

--- core/static/core/js/movie.js
+++ core/static/core/js/movie.js
@@ -1,15 +1,4 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const modals = document.querySelectorAll(".modal");
-//     modals.forEach((modal) => {
-//       modal.addEventListener("hidden.bs.modal", (event) => {
-//         const video = modal.querySelector("video");
-//         if (video) {
-//           video.pause(); 
-//           video.currentTime = 0; 
-//         }
-//       });
-//     });
-//   });
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
